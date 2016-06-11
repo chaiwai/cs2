15c263d938da26af42d1ba7dc093a43f35e8c616
chaiwai <dan.w.landolfi@gmail.com>
2016-06-11 16:14:16 -0400

    commit

--- hw3/Treap.java
+++ hw3/Treap.java
@@ -2,7 +2,18 @@
 
 //Implement the probabilistic Treap data structure. It is important that you implement the insertion and deletion operations as described in class. (In particular, be sure to read my note in Webcourses on how deletion is handled, which includes a discussion of how this differs from our traditional approach to BST deletion.) Your Treap class must be generic. Since there is an ordering property in treaps, you must also restrict the type parameter (e.g., AnyType) to classes that implement Comparable.
 
-public class Treap
+class Node<T>
+{
+	T data;
+	Node<T> left, right;
+	
+	Node(T data)
+	{
+		this.data = data;
+	}
+}
+
+public class Treap<T extends Comparable<T>>
 {
 //	Add data to the treap. Do not allow insertion of duplicate values. You will have to generate a
 //	random priority for this node. Find an efficient way to ensure that no duplicate priorities are
@@ -29,28 +40,31 @@ public class Treap
 //	Return true if the treap contains data, false otherwise.
 	public boolean contains(T data)
 	{
-	
-	{
+		
+		return false;
+	}
 	
 //	Return the number of elements in the treap in O(1) time.
 	public int size()
 	{
-	
+		
+		return 0; // change	
 	}
 //	Return the height of the treap. (An O(n) solution is fine here.)
 	public int height()
 	{
 	
+		return 0; // change
 	}
 	
 	public static double difficultyRating()
 	{
-	
+		return 3.0; // change
 	}
 	
 	public static double hoursSpent()
 	{
-	
+		return 3.0; // change
 	}
 	
 
