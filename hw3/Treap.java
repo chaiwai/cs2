3f2c01b7c48742180a3ef5c0dcac481ec5de1190
chaiwai <dan.w.landolfi@gmail.com>
2016-06-11 14:05:36 -0400

    starting hw3

---
+++ hw3/Treap.java
@@ -0,0 +1,7 @@
+// info
+
+//Implement the probabilistic Treap data structure. It is important that you implement the insertion and deletion operations as described in class. (In particular, be sure to read my note in Webcourses on how deletion is handled, which includes a discussion of how this differs from our traditional approach to BST deletion.) Your Treap class must be generic. Since there is an ordering property in treaps, you must also restrict the type parameter (e.g., AnyType) to classes that implement Comparable.
+
+public class Treap{
+
+}
