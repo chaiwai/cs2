6986fb78a33abf50732d63802a3e18f06d4df7cb
chaiwai <dan.w.landolfi@gmail.com>
2016-06-17 18:47:46 -0400

    commit

--- hw3/Treap.java
+++ hw3/Treap.java
@@ -6,24 +6,30 @@ class Node<T>
 {
 	T data;
 	Node<T> left, right;
+	int priority;
 	
-	Node(T data)
+	Node(T data, int priority)
 	{
 		this.data = data;
+		this.priority = priority;
 	}
 }
 
 public class Treap<T extends Comparable<T>>
 {
 	private int size = 0;
-	private Node<T> root;	
+	private Node<T> root;
+	//hashset maybe?
+	
+	//constructor?
 	
 //	Add data to the treap. Do not allow insertion of duplicate values. You will have to generate a
 //	random priority for this node. Find an efficient way to ensure that no duplicate priorities are
 //	generated, which uses no more than O(n) extra memory.
 	public void add(T data)
 	{
-	
+		
+		// some rand numb gen
 	}
 	
 //	Insert data into the treap (as above), but do not generate a random node priority for the new
@@ -34,17 +40,32 @@ public class Treap<T extends Comparable<T>>
 	
 	}
 	
+	private Node<T> add(Node<T> root, T data, int priority)
+	{
+		return null;  //for now
+	}
+	
 //	Delete data from the treap (if it is present).
 	public void remove(T data)
 	{
 	
 	}
 	
+	private Node<T> delete(Node<T> root, T data)
+	{
+		return null; //for now
+	}
+	
 //	Return true if the treap contains data, false otherwise.
 	public boolean contains(T data)
 	{
-		
-		return false; // change
+		return contains(root, data); // change
+	}
+	
+	private boolean contains(Node<T> root, T data)
+	{
+		return false;
+		// cp from genericbst?
 	}
 	
 //	Return the number of elements in the treap in O(1) time.
